d045dc1a5855e9c0b80d9abe45b414ffeeff5a86
Kernel114514 <Kernel114514@hotmail.com>
2025-09-21 20:35:04 +0800

    mt6875-common: Define bootloader and platform flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,5 +21,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+BOARD_VENDOR := xiaomi
+TARGET_BOOTLOADER_BOARD_NAME := mt6875
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6875
+
 # Inherit the proprietary files
 include vendor/xiaomi/mt6875-common/BoardConfigVendor.mk
