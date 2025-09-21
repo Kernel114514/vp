9b0cda5cc0439954a443c2637f2201470f1959d9
Kernel114514 <Kernel114514@hotmail.com>
2025-09-21 20:27:36 +0800

    mt6875-common: Initial skeleton

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/mt6875-common
+
+# Inherit the proprietary files
+include vendor/xiaomi/mt6875-common/BoardConfigVendor.mk
