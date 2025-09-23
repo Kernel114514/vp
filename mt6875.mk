f25749d5dcbcd0d8c4c6baf166af5ff062477272
Kernel114514 <Kernel114514@hotmail.com>
2025-09-23 11:15:52 +0800

    Build required Graphics interfaces/libs

--- mt6875.mk
+++ mt6875.mk
@@ -51,6 +51,18 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-service
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.common@1.2.vendor \
+    android.hardware.graphics.mapper@4.0.vendor \
+    android.hardware.graphics.allocator@4.0.vendor
+
+PRODUCT_PACKAGES += \
+    libdrm.vendor
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
